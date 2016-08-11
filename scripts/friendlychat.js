b457c6b2fa62ec4e9a79d115738c0ee363eef155
Maggie Neterval <mneterval@gmail.com>
2016-08-11 18:40:43 -0400

    ashi config

--- scripts/friendlychat.js
+++ scripts/friendlychat.js
@@ -15,9 +15,18 @@
  */
 'use strict';
 
+var firebase = require('firebase');
+
+var config = {
+    apiKey: "AIzaSyAGlJNi77LCyxRye_4--6FM-sAP4uRFccM",
+    authDomain: "shhh-lack.firebaseapp.com",
+    databaseURL: "https://shhh-lack.firebaseio.com",
+    storageBucket: "shhh-lack.appspot.com",
+  };
+  firebase.initializeApp(config);
+
 // Initializes FriendlyChat.
 function FriendlyChat() {
-  this.checkSetup();
 
   // Shortcuts to DOM Elements.
   this.messageList = document.getElementById('messages');
@@ -306,21 +315,6 @@ FriendlyChat.prototype.toggleButton = function() {
     this.submitButton.removeAttribute('disabled');
   } else {
     this.submitButton.setAttribute('disabled', 'true');
-  }
-};
-
-// Checks that the Firebase SDK has been correctly setup and configured.
-FriendlyChat.prototype.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions.');
-  } else if (config.storageBucket === '') {
-    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
-        'actually a Firebase bug that occurs rarely. ' +
-        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
-        'and make sure the storageBucket attribute is not empty. ' +
-        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
-        'displayed there.');
   }
 };
 
