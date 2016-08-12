e0907d1b31148f38c30c2360074b48c9f7133093
Maggie Neterval <mneterval@gmail.com>
2016-08-12 14:51:59 -0400

    angular dep

--- scripts/states/create_room.state.js
+++ scripts/states/create_room.state.js
@@ -1,2 +1,12 @@
 var app = require('../app.js');
 
+app.config(function ($stateProvider) {
+
+  $stateProvider.state('createRoom', {
+    url: '/create-room',
+    templateUrl: './test.html',
+    controller: function ($scope) {
+      $scope.test = "Testing";
+    }
+  });
+});
