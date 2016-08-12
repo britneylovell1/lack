ea1d684e7661e84a9e25a499526405b035bb0319
Britney Lovell <britneyklovell@gmail.com>
2016-08-12 15:26:34 -0400

    initialized create-your-team feature

---
+++ scripts/controllers/create_your_team.controller.js
@@ -0,0 +1,5 @@
+var app = require('../app.js');
+
+app.controller('createTeamCtrl', function($scope) {
+	
+})
---
+++ scripts/factories/create_team.factory.js
@@ -0,0 +1,5 @@
+var app = require('../app.js');
+
+app.factory('teamFactory', function() {
+
+})
