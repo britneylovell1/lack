0cfe4edc5172b3d08e71a3b7ff5b6e04945a6834
Britney Lovell <britneyklovell@gmail.com>
2016-08-13 16:38:01 -0400

    login wired up to firebase

--- scripts/states/login.state.js
+++ scripts/states/login.state.js
@@ -5,8 +5,24 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('login', {
     url: '/login',
     templateUrl: '../templates/login.html',
-    controller: function ($scope) {
+    controller: function ($scope, $firebaseAuth) {
+    	// testing the controller
       $scope.test = "login page";
+
+	    $scope.signIn = function() {
+				// Sign in Firebase using popup auth and Google as the identity provider.
+				$scope.authObj = $firebaseAuth();
+
+				$scope.authObj.$signInWithPopup("google")
+				.then(function(result) {
+					console.log("Signed in as:", result.user.uid);
+				})
+				.catch(function(error) {
+					console.error("Authentication failed:", error);
+				});
+			}
+
+			// still need to redirect to different state
     }
   });
 };
