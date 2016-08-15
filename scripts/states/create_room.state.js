a01736c212508426e35d0283da216b5628da06b5
Britney Lovell <britneyklovell@gmail.com>
2016-08-15 11:54:50 -0400

    user setup

--- scripts/states/create_room.state.js
+++ scripts/states/create_room.state.js
@@ -5,8 +5,43 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('createRoom', {
     url: '/create-room',
     templateUrl: '/templates/create_room.html',
-    controller: function ($scope) {
-      $scope.allMembers = ['Maggie', 'Britney', 'Elisabeth'];
-    }
+    // controller: function ($scope) {
+    //   $scope.allMembers = ['Maggie', 'Britney', 'Elisabeth'];
+    // }
+    // future task: modularize the controller
+	controller: function($scope, $firebaseArray, $firebaseObject, $state) {
+		// TODO:
+		// create a team in firebase
+		// clear input field + reset $scope.team
+		// email team members (figure out how to do this)
+		// set admin status for current user
+		// associate each member (including current user) to the team
+		// redirect to that team's state
+
+		function createTeam() {
+	      // create the team obj in firebase + get the reference to it
+	      var newTeamRef = firebase.database().ref('teams').push();
+
+	      // return the team as a synchronized object
+	      return $firebaseObject(newTeamRef);
+	    }
+
+	    // this should be the new team obj.
+		$scope.team = createTeam();
+
+		// save team.name and team.members in firebase
+		// figure out how to parse/save the members with Material chips
+		$scope.saveTeam = function() {
+			$scope.team.emails = $scope.emails;
+			$scope.team.$save().then(function() {
+		        alert('Team saved!');
+		        $state.go('home');
+		      }).catch(function(error) {
+		        alert('Error!');
+		      });
+		};
+		//initialize empty array for Angular Material chips:
+		$scope.emails = [];
+	}
   });
 };
--- scripts/states/login.state.js
+++ scripts/states/login.state.js
@@ -5,9 +5,9 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('login', {
     url: '/login',
     templateUrl: '../templates/login.html',
-    controller: function ($scope, $firebaseAuth) {
+    controller: function ($scope, $firebaseAuth, $firebaseObject) {
     	// testing the controller
-      $scope.test = "login page";
+      // $scope.test = "login page";
 
 	    $scope.signIn = function() {
 				// Sign in Firebase using popup auth and Google as the identity provider.
@@ -16,13 +16,26 @@ module.exports = function ($stateProvider) {
 				$scope.authObj.$signInWithPopup("google")
 				.then(function(result) {
 					console.log("Signed in as:", result.user.uid);
+					var userId = result.user.uid;
+					
+					// create new user or login the already created user
+					function createUser() {
+				      // create the user obj in firebase + get the reference to it
+				      var newUserRef = firebase.database().ref('users').child(userId);
+
+				      // return the team as a synchronized object
+				      return $firebaseObject(newUserRef);
+				    }
+
+
+
 				})
 				.catch(function(error) {
 					console.error("Authentication failed:", error);
 				});
-			}
+		}
 
-			// still need to redirect to different state
+		// still need to redirect to different state
     }
   });
 };
