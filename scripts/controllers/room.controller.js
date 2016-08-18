3106928956208a36956cce46cfac070ed46f6627
Britney Lovell <britneyklovell@gmail.com>
2016-08-18 12:20:01 -0400

    refactored controllers/ added room-team assoc

---
+++ scripts/controllers/room.controller.js
@@ -0,0 +1,44 @@
+var angular = require('angular');
+var app = angular.module('lack');
+
+module.exports = function($log, $rootScope, $scope, $state, $firebaseObject, roomFactory, UserFactory, teamMembers, AssocFactory) {
+  // TODO:
+  // get current team from url
+
+  // Reformat teamMembers array for the query
+  var members = roomFactory.members(teamMembers);
+  console.log(members);
+
+  $scope.querySearch = function(query) {
+    var results = query ? members.filter(roomFactory.createFilterFor(query)) : null;
+    return results;
+  };
+
+  // create the room
+  $scope.room = roomFactory.createRoom();
+  $scope.user = UserFactory.getCurrentUser();
+
+  // we'd want to lookup the members in the database and then send those with the room to the database
+  $scope.chipsmembers = [];
+
+  $scope.saveRoom = function() {
+    // $scope.room.members = $scope.chipsmembers;
+    $scope.room.$save().then(function() {
+      alert('Room created!');
+
+      // create user-room associations
+      $scope.chipsmembers.forEach(function(member) {
+        console.log(member);
+        AssocFactory.assocUserRoom(member, $scope.room)
+      })
+
+      // make this user the admin
+      roomFactory.addRoomAdmin($scope.user, $scope.room)
+
+      $state.go('home');
+
+    }).catch(function(error) {
+      alert('Error!');
+    });
+  };
+}
--- scripts/states/create_your_team.state.js
+++ scripts/states/create_your_team.state.js
@@ -1,67 +1,13 @@
 var angular = require('angular');
 var app = angular.module('lack');
-var firebase = require('firebase');
+var TeamController = require('../controllers/team.controller.js');
 
 
 var createTeamState = function($stateProvider){
 	$stateProvider.state('createYourTeam', {
 		url: '/create-team',
 		templateUrl: '../templates/create_your_team.html',
-
-		// future task: modularize the controller
-		controller: function($rootScope, $scope, $firebaseArray, $firebaseObject, $state, EmailFactory, UserFactory, TeamFactory) {
-			// TODO:
-			// sign-in pop-up bugs
-
-
-	    // set $scope.team to new team obj (but do not bind)
-			var teamObj = TeamFactory.createTeam();
-			$scope.team = teamObj;
-
-			
-	    
-
-			// save team.name and team.members in firebase
-			$scope.saveTeam = function() {
-
-				$scope.team.emails = $scope.emails;
-
-				EmailFactory.sendInvitations($scope.team);
-
-				$scope.team.$save().then(function() {
-	        alert('Team saved!');
-
-	        // bind the team obj to the rootScope.teamObj
-					teamObj.$loaded().then(function () {
-			      teamObj.$bindTo($rootScope, 'teamObj').then(function () {
-			        console.log('$rootScope.teamObj ', $rootScope.teamObj);
-			      });
-			    });
-
-	        UserFactory.signIn()
-		        .then(function(user) {
-
-		          // associate user with team 
-		          return TeamFactory.assocUserTeam(user, $scope.team)
-		        })
-		        .then(function(user) {
-
-		          // set this user as the admin
-		          TeamFactory.addTeamAdmin(user, $scope.team);
-			        $state.go('home');
-		        });
-
-		      }).catch(function(error) {
-		        alert('Error!');
-		      });
-			};
-
-			  
-
-			//initialize empty array for Angular Material chips:
-			$scope.emails = [];
-
-		}
+		controller: TeamController
 	})
 }
 
