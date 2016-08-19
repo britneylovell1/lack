653591b8c335451c36cd6e726bde5dd66350dd27
Elisabeth Gross <elisabethgross568@gmail.com>
2016-08-19 11:05:29 -0400

    rooms on sidebar are real

--- scripts/directives/sidebar.directive.js
+++ scripts/directives/sidebar.directive.js
@@ -1,16 +1,25 @@
 var angular = require('angular');
 var app = angular.module('lack');
+var firebase = require('firebase');
 
-module.exports = function ($state) {
+module.exports = function($state, $firebaseArray, $rootScope) {
   return {
     restrict: 'E',
     templateUrl: '../templates/sidebar.html',
     scope: {
-     rooms: '=',
-     currentTeamId: '='
+      rooms: '=',
+      currentTeamId: '='
     },
-    link: function (scope, element, attrs) {
+    link: function(scope) {
+      function getRooms() {
+        var roomsRef = firebase.database().ref('rooms');
+        return $firebaseArray(roomsRef);
+      }
+      scope.rooms = getRooms();
 
+      scope.setCurrentRoom = function(room) {
+        $rootScope.currentRoom = room;
+      };
     }
   };
 };
