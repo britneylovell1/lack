13cdf461f50e35108350fa79cf2ffdcfe228cf0e
Britney Lovell <britneyklovell@gmail.com>
2016-08-19 11:31:26 -0400

    cleaned up user factory

--- scripts/factories/association.factory.js
+++ scripts/factories/association.factory.js
@@ -2,6 +2,9 @@ var angular = require('angular');
 var app = angular.module('lack');
 var firebase = require('firebase');
 
+// TODO:
+// associate user and messages
+
 module.exports = function($firebaseArray, $firebaseObject) {
 
 	return {
@@ -10,18 +13,14 @@ module.exports = function($firebaseArray, $firebaseObject) {
 
 			// set up association variables
 			var userInfo = {
-				// userId: user.uid,		// This was for the first attempt
-				// userName: user.displayName
+				[user.uid]: { userName: user.displayName }
 			};
-			userInfo[user.uid] = { userName: user.displayName };
 
+			// set up association variables
 			var teamId = team.id || team.$id;
 			var teamInfo = {
-				// teamId: teamId,			// This was for the first attempt
-				// teamName: team.name
+				[teamId]: { teamName: team.name }
 			};
-			teamInfo[teamId] = { teamName: team.name };
-
 
 			// create user + team entries and set up references to them
 			var userRef = firebase.database().ref().child('users/' + user.uid + '/teams');
@@ -36,11 +35,6 @@ module.exports = function($firebaseArray, $firebaseObject) {
 				userRef.update(teamInfo);
 				teamRef.update(userInfo);
 
-				// This was for the first attempt
-    		// This is method created a new id for each $add()
-				// $firebaseArray(userRef).$add(teamInfo);
-				// $firebaseArray(teamRef).$add(userInfo);
-
 			})
 
 			return user;
@@ -51,19 +45,16 @@ module.exports = function($firebaseArray, $firebaseObject) {
 			// Do we even need to add 'teams/team.id/rooms'???
 
 	    // set up association variables
-	    var teamId = team.id || team.$id
-	    var teamInfo = {
-	      // teamId: teamId,      // This was for the first attempt
-	      // teamName: team.name
-	    };
-	    teamInfo[teamId] = { teamName: team.name };
+	    var teamId = team.id || team.$id;
+			var teamInfo = {
+				[teamId]: { teamName: team.name }
+			};
 
-	    var roomId = room.id || room.$id
+	    var roomId = room.id || room.$id;
 	    var roomInfo = {
-	      // roomId: roomId,      // This was for the first attempt
-	      // roomName: room.name
+	      [roomId]: { roomName: room.name }
 	    };
-	    roomInfo[roomId] = { roomName: room.name }
+	    
 
 	    // create team + room entries and set up references to them
 	    var teamRef = firebase.database().ref().child('teams/' + teamId + '/rooms');
@@ -80,18 +71,14 @@ module.exports = function($firebaseArray, $firebaseObject) {
 	  assocUserRoom: function(user, room) {
 
 	    // set up association variables
-	    var userInfo = {
-	      // userId: user.uid,      // This was for the first attempt
-	      // userName: user.displayName
-	    };
-	    userInfo[user.userId] = { userName: user.display };
+			var userInfo = {
+				[user.uid]: { userName: user.displayName }
+			};
 
-	    var roomId = room.id || room.$id
+	    var roomId = room.id || room.$id;
 	    var roomInfo = {
-	      // roomId: roomId,      // This was for the first attempt
-	      // roomName: room.name
+	      [roomId]: { roomName: room.name }
 	    };
-	    roomInfo[roomId] = { roomName: room.name }
 
 	    // create user + room entries and set up references to them
 	    var userRef = firebase.database().ref().child('users/' + user.userId + '/rooms');
@@ -101,17 +88,12 @@ module.exports = function($firebaseArray, $firebaseObject) {
 	    userRef.update(roomInfo);
 	    roomRef.update(userInfo);
 
-	    // This was for the first attempt
-	    // This is method created a new id for each $add()
-	    // $firebaseArray(userRef).$add(roomInfo);
-	    // $firebaseArray(roomRef).$add(userInfo);
-
 	    return user;
 
 	  },
 
 	  assocRoomMessage: function() {
-
+	  	// do this
 	  }
 		
 	}
