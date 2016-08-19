e14934a3d3153ce9547954441192e98ca11015a1
Maggie Neterval <mneterval@gmail.com>
2016-08-19 14:25:15 -0400

    changed CSS form class

--- scripts/states/close_room.state.js
+++ scripts/states/close_room.state.js
@@ -6,22 +6,33 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('closeRoom', {
     url: '/home/teams/:teamId/rooms/:roomId/close-room',
     templateUrl: '/templates/close_room.html',
-    controller: function ($scope, $state, $mdToast, EmailFactory, $stateParams) {
+    resolve: {
+      roomMembers: function (TeamFactory, $stateParams) {
+        return TeamFactory.getTeamMembers($stateParams.teamId);
+      },
+      roomData: function ($stateParams) {
+        return firebase.database().ref('rooms/' + $stateParams.roomId);
+      }
+    },
+    controller: function ($scope, $state, $mdToast, EmailFactory, $stateParams, TeamFactory, roomMembers, roomData) {
 
       //grab team and room IDs from URL params:
       $scope.teamId = $stateParams.teamId;
       $scope.roomId = $stateParams.roomId;
 
       //TODO: fetch actual Room from firebase:
-      $scope.currentRoom = { name: "ExampleCurrentRoom" };
+      $scope.currentRoom = { name: 'ExampleCurrentRoom' };
 
       //TODO: fetch all team members and their emails
-      $scope.allMembers = ['Britney', 'Maggie', 'Elisabeth'];
+      $scope.allMembers = roomMembers;
+      //have .userName and .$id properties
 
       //TODO: grab notes and member info from form
       $scope.data = {};
 
       $scope.closeRoom = function () {
+
+        //TODO: fetch email address of each selected member and add to data obj.
 
         //TODO: finish this factory function
         EmailFactory.sendRoomNotes($scope.data)
