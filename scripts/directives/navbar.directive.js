ae7b3ad0f1f181d7f513ce2cc56965ca0d2bdbdc
Britney Lovell <britneyklovell@gmail.com>
2016-08-21 12:46:50 -0400

    team id persists in url

--- scripts/directives/navbar.directive.js
+++ scripts/directives/navbar.directive.js
@@ -2,7 +2,7 @@ var angular = require('angular');
 var app = angular.module('lack');
 var firebase = require('firebase');
 
-module.exports = function ($state, AdminUserFactory) {
+module.exports = function ($state, AdminUserFactory, TeamFactory) {
 
   return {
 
@@ -13,13 +13,28 @@ module.exports = function ($state, AdminUserFactory) {
 
       scope.loggedIn = false;
 
+      scope.goHome = function() {
+        TeamFactory.getCurrentTeam()
+          .then(function(team){
+            $state.go('home', {teamId: team.$id});
+          });
+      }
+      scope.log = function() {
+        console.log('it works')
+      }
+
       firebase.auth().onAuthStateChanged(function (user) {
+        // This prevents a user from creating a new team when they're signed in
         if (user) {
           scope.loggedIn = true;
+
+          scope.goHome();
+
           //TODO: FIX ADMIN CHECK
           // $rootScope.isAdmin = AdminUserFactory.checkIfAdmin(user.uid, scope.team, false);
         } else {
           scope.loggedIn = false;
+          $state.go('landing');
         }
       });
 
