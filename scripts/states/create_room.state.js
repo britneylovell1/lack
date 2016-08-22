278c83c7512efd834498ed58f3d049eb3b4124a2
Maggie Neterval <mneterval@gmail.com>
2016-08-22 17:46:59 -0400

    removing comment

--- scripts/states/create_room.state.js
+++ scripts/states/create_room.state.js
@@ -6,15 +6,6 @@ module.exports = function($stateProvider) {
   $stateProvider.state('createRoom', {
     url: '/create-room',
     templateUrl: '/templates/create_room.html',
-    // resolve: {
-    //   teamMembers: function() {
-    //     return TeamFactory.getCurrentTeamId()
-    //     .then(function(teamId) {
-    //       return TeamFactory.getTeamMembers(teamId).$loaded();
-          
-    //     })
-    //   }
-    // },
     controller: RoomController
   });
 };
